814d885bc795ec9e525533fa0777dd627353eac5
Eli Sacks <bastlifa@gmail.com>
2019-06-21 11:50:20 -0400

    Completed stretch, no animation, just cycles through

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,40 @@
 class Carousel {
+    constructor(carousel)
+    {
+        this.carousel = carousel;
+        this.leftBtn = carousel.querySelector(".left-button");
+        this.rightBtn = carousel.querySelector(".right-button");
+        this.aImages = Array.from(carousel.querySelectorAll("img"));
+        this.currentIndex = 0;
 
+        window.addEventListener("load", () => this.aImages[0].style.display = "block")
+        this.leftBtn.addEventListener("click", () => this.goLeft());
+        this.rightBtn.addEventListener("click", () => this.goRight());
+        // console.log("lftbtn", this.leftBtn);
+        // console.log(this.aImages)
+    }
+    goLeft()
+    {
+        console.log(this.aImages)
+        if (this.currentIndex == 0) this.currentIndex = this.aImages.length - 1;
+        else                       this.currentIndex--;
+        this.aImages.forEach(img => img.style.display = "none");
+        this.aImages[this.currentIndex].style.display = "block";
+    }
+    goRight()
+    {
+        console.log(this.aImages)
+        if (this.currentIndex == this.aImages.length - 1) this.currentIndex = 0;
+        else                       this.currentIndex++;
+        this.aImages.forEach(img => img.style.display = "none");
+        this.aImages[this.currentIndex].style.display = "block";
+    }
 }
 
-let carousel = document.querySelector();
+document.querySelectorAll("img").forEach(img => img.addEventListener("click", () => event.preventDefault()));
 
+let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
